7a59655902c0f6d742f2de0cd4e119a241e27371
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-09-11 11:43:22 -0300

    Database Neon configured

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -m "Next subject: Database (use Neon?)"
+git commit -m "Database Neon configured"
 git push -u origin main
 git status
 
-# continue in: https://youtu.be/hHM-hr9q4mo?si=LBR7QIjeQzHgoiHk&t=3225
+# continue in: https://youtu.be/hHM-hr9q4mo (58:15)
